f1e890cfc720466a867eb183cc75efd882191975
hueyp11 <118989093+hueyp11@users.noreply.github.com>
2023-03-19 22:15:11 -0400

    Created variables to keep score for game(). Just need to create function to declare winner.

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -1,5 +1,6 @@
 let rockPaperScissors = ["Rock", "Paper", "Scissors"];
-
+var playerWins = 0;
+var computerWins = 0;
 
 function getComputerChoice() {
     let choice = rockPaperScissors[Math.floor(Math.random()*rockPaperScissors.length)];
@@ -16,21 +17,27 @@ function game() {
             var computerSelection = getComputerChoice();
 
 
-            if (playerSelection == 'rock' && computerSelection == 'Rock') {
+            if (playerSelection == 'rock' && computerSelection == 'Rock') {                   
                     return console.log("This round ends in a tie.");
                 }   else if (playerSelection == 'rock' && computerSelection == 'Paper') {
+                    computerWins = computerWins + 1;
                     return console.log("You lose! Paper beats rock.");
                 }   else if (playerSelection == 'rock' && computerSelection == 'Scissors') {
+                    playerWins = playerWins + 1;
                     return console.log("You win! Rock beats scissors.");
                 }   else if (playerSelection == 'paper' && computerSelection == 'Rock') {
+                    playerWins = playerWins + 1
                     return console.log("You win! Paper beats rock.");
                 }   else if (playerSelection == 'paper' && computerSelection == 'Paper') {
                     return console.log("This round ends in a tie.");
                 }   else if (playerSelection == 'paper' && computerSelection == 'Scissors') {
+                    computerWins = computerWins + 1;
                     return console.log("You lose! Scissors beats paper.");
                 }   else if (playerSelection == 'scissors' && computerSelection == 'Rock') {
+                    computerWins = computerWins + 1;
                     return console.log("You lose! Rock beats scissors.");
                 }   else if (playerSelection == 'scissors' && computerSelection == 'Paper') {
+                    playerWins = playerWins + 1
                     return console.log("You win! Scissors beats paper.");
                 }   else (playerSelection == 'scissors' && computerSelection == 'Scissors'); {
                     return console.log("This round ends in a tie.");
@@ -38,8 +45,9 @@ function game() {
         }
 
         singleRoundOfRps();
-
+        console.log("Player: " + playerWins + " " + "Computer: " + computerWins);
     }
 }
 
 game();
+

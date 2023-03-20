a43212f266b50d98ac451b9dc3fe5df8df97a8e3
hueyp11 <118989093+hueyp11@users.noreply.github.com>
2023-03-19 20:22:17 -0400

    game() completed. Need helper function to declare winner.

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -7,44 +7,39 @@ function getComputerChoice() {
 }
 
 
+function game() {
+    for (let i = 0; i < 5; i++) {
+        function singleRoundOfRps(playerSelection, computerSelection) {
+            
+            var playerSelection = prompt("Enter your choice of Rock, Paper, or Scissors!")
+            playerSelection = playerSelection.toLowerCase();
+            var computerSelection = getComputerChoice();
 
-function singleRoundOfRps(playerSelection, computerSelection) {
-    
-    var playerSelection = prompt("Enter your choice of Rock, Paper, or Scissors!")
-    playerSelection = playerSelection.toLowerCase();
-    var computerSelection = getComputerChoice();
 
-
-    if (playerSelection == 'rock' && computerSelection == 'Rock') {
-            return "This round ends in a tie.";
-        }   else if (playerSelection == 'rock' && computerSelection == 'Paper') {
-            return "You lose! Paper beats rock.";
-        }   else if (playerSelection == 'rock' && computerSelection == 'Scissors') {
-            return "You win! Rock beats scissors.";
-        }   else if (playerSelection == 'paper' && computerSelection == 'Rock') {
-            return "You win! Paper beats rock.";
-        }   else if (playerSelection == 'paper' && computerSelection == 'Paper') {
-            return "This round ends in a tie.";
-        }   else if (playerSelection == 'paper' && computerSelection == 'Scissors') {
-            return "You lose! Scissors beats paper.";
-        }   else if (playerSelection == 'scissors' && computerSelection == 'Rock') {
-            return "You lose! Rock beats scissors.";
-        }   else if (playerSelection == 'scissors' && computerSelection == 'Paper') {
-            return "You win! Scissors beats paper.";
-        }   else (playerSelection == 'scissors' && computerSelection == 'Scissors'); {
-            return "This round ends in a tie.";
+            if (playerSelection == 'rock' && computerSelection == 'Rock') {
+                    return console.log("This round ends in a tie.");
+                }   else if (playerSelection == 'rock' && computerSelection == 'Paper') {
+                    return console.log("You lose! Paper beats rock.");
+                }   else if (playerSelection == 'rock' && computerSelection == 'Scissors') {
+                    return console.log("You win! Rock beats scissors.");
+                }   else if (playerSelection == 'paper' && computerSelection == 'Rock') {
+                    return console.log("You win! Paper beats rock.");
+                }   else if (playerSelection == 'paper' && computerSelection == 'Paper') {
+                    return console.log("This round ends in a tie.");
+                }   else if (playerSelection == 'paper' && computerSelection == 'Scissors') {
+                    return console.log("You lose! Scissors beats paper.");
+                }   else if (playerSelection == 'scissors' && computerSelection == 'Rock') {
+                    return console.log("You lose! Rock beats scissors.");
+                }   else if (playerSelection == 'scissors' && computerSelection == 'Paper') {
+                    return console.log("You win! Scissors beats paper.");
+                }   else (playerSelection == 'scissors' && computerSelection == 'Scissors'); {
+                    return console.log("This round ends in a tie.");
+                }    
         }
-    
+
+        singleRoundOfRps();
+
+    }
 }
 
-//console.log(singleRoundOfRps());
-
-
-//function game() {
-//    for (let i = 0; i < 5; i++) {
-//        singleRoundOfRps();
-//        return singleRoundOfRps();
-//    }
-//}
-
-//game();
+game();

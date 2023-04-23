df139970fa0e7072260c941d6d7d4ef7658a5a8d
hueyp11 <118989093+hueyp11@users.noreply.github.com>
2023-04-23 14:27:28 -0400

    The game has now been completed and is functioning properly. I only need to design the UI and figure out how to kill the program after winner declared.

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -2,12 +2,6 @@
 let rockPaperScissors = ["Rock", "Paper", "Scissors"];
 var playerWins = 0;
 var computerWins = 0;
-
-function getComputerChoice() {
-    let choice = rockPaperScissors[Math.floor(Math.random()*rockPaperScissors.length)];
-    return choice
-}
-
 var playerSelection = document.querySelectorAll('button');
 playerSelection.forEach((button) => {
 button.addEventListener('click', (e) => {
@@ -15,59 +9,55 @@ button.addEventListener('click', (e) => {
 });
 });
 
+function getComputerChoice() {
+    let choice = rockPaperScissors[Math.floor(Math.random()*rockPaperScissors.length)];
+    return choice
+}
+
 function singleRoundOfRps(playerSelection, computerSelection) {
 
     var computerSelection = getComputerChoice();
 
     function declareWinner() {
     if (playerWins === 5) {
-        return console.log("You win!");
+        return document.getElementById('results').innerHTML = "You win!";
     } else if (computerWins == 5) {
-        return console.log("You lose! Computer wins!"); 
+        return document.getElementById('results').innerHTML = "You lose! Computer wins!"; 
     }
     }
 
     if (playerSelection == 'rock' && computerSelection == 'Rock') { 
-            console.log("Player: " + playerWins + " " + "Computer: " + computerWins);                 
-            console.log("This round ends in a tie.");
-            return declareWinner()
+            document.getElementById('results').innerHTML = `This round ends in a tie. <br /> Player: ${playerWins} Computer: ${computerWins}`;                 
+            return declareWinner();
         }   else if (playerSelection == 'rock' && computerSelection == 'Paper') {
             computerWins = computerWins + 1;
-            console.log("Player: " + playerWins + " " + "Computer: " + computerWins);
-            console.log("You lose! Paper beats rock.");
+            document.getElementById('results').innerHTML = `You lose! Paper beats rock. <br /> Player: ${playerWins} Computer: ${computerWins}`;                 
             return declareWinner()
         }   else if (playerSelection == 'rock' && computerSelection == 'Scissors') {
             playerWins = playerWins + 1;
-            console.log("Player: " + playerWins + " " + "Computer: " + computerWins);
-            console.log("You win! Rock beats scissors.");
+            document.getElementById('results').innerHTML = `You win! Rock beats scissors. <br /> Player: ${playerWins} Computer: ${computerWins}`;                 
             return declareWinner()
         }   else if (playerSelection == 'paper' && computerSelection == 'Rock') {
             playerWins = playerWins + 1
-            console.log("Player: " + playerWins + " " + "Computer: " + computerWins);
-            console.log("You win! Paper beats rock.");
+            document.getElementById('results').innerHTML = `You win! Paper beats rock. <br /> Player: ${playerWins} Computer: ${computerWins}`;                 
             return declareWinner()
         }   else if (playerSelection == 'paper' && computerSelection == 'Paper') {
-            console.log("Player: " + playerWins + " " + "Computer: " + computerWins);
-            console.log("This round ends in a tie.");
+            document.getElementById('results').innerHTML = `This round ends in a tie. <br /> Player: ${playerWins} Computer: ${computerWins}`;                 
             return declareWinner()
         }   else if (playerSelection == 'paper' && computerSelection == 'Scissors') {
             computerWins = computerWins + 1;
-            console.log("Player: " + playerWins + " " + "Computer: " + computerWins);
-            console.log("You lose! Scissors beats paper.");
+            document.getElementById('results').innerHTML = `You lose! Scissors beats paper. <br /> Player: ${playerWins} Computer: ${computerWins}`;                 
             return declareWinner()
         }   else if (playerSelection == 'scissors' && computerSelection == 'Rock') {
             computerWins = computerWins + 1;
-            console.log("Player: " + playerWins + " " + "Computer: " + computerWins);
-            console.log("You lose! Rock beats scissors.");
+            document.getElementById('results').innerHTML = `You lose! Rock beats scissors. <br /> Player: ${playerWins} Computer: ${computerWins}`;                 
             return declareWinner()
         }   else if (playerSelection == 'scissors' && computerSelection == 'Paper') {
             playerWins = playerWins + 1
-            console.log("Player: " + playerWins + " " + "Computer: " + computerWins);
-            console.log("You win! Scissors beats paper.");
+            document.getElementById('results').innerHTML = `You win! Scissors beats paper. <br /> Player: ${playerWins} Computer: ${computerWins}`;                 
             return declareWinner()
         }   else (playerSelection == 'scissors' && computerSelection == 'Scissors'); {
-            console.log("Player: " + playerWins + " " + "Computer: " + computerWins);
-            console.log("This round ends in a tie.");
+            document.getElementById('results').innerHTML = `This round ends in a tie. <br /> Player: ${playerWins} Computer: ${computerWins}`;                 
             return declareWinner()
         }
           
